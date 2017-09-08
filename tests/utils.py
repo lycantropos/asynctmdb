ea12c512318f131b6e2d9e0253d3739444c4afaf
Azat Ibrakov <azatibrakov@gmail.com>
2017-09-08 19:32:41 +0700

    Added checking for paginated record

--- tests/utils.py
+++ tests/utils.py
@@ -1,4 +1,6 @@
-from typing import Any
+from typing import (Any,
+                    Iterable,
+                    Dict)
 
 from hypothesis import (Verbosity,
                         find,
@@ -19,5 +21,23 @@ def is_positive_integer(value: Any) -> bool:
     return isinstance(value, int) and value > 0
 
 
+def is_non_negative_integer(value: Any) -> bool:
+    return isinstance(value, int) and value >= 0
+
+
 def is_non_empty_string(value: Any) -> bool:
     return isinstance(value, str) and value
+
+
+def is_valid_paginated_record(
+        record: Dict[str, Any],
+        paginated_record_keys: Iterable[str] = ('page',
+                                                'total_pages',
+                                                'total_results')) -> bool:
+    if not isinstance(record, dict):
+        return False
+    if any(key not in record for key in paginated_record_keys):
+        return False
+    return (is_positive_integer(record['page']) and
+            is_non_negative_integer(record['total_pages']) and
+            is_non_negative_integer(record['total_results']))
