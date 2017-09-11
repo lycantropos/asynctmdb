44d84f974a56cadb547574f59b69f11f07a794f8
ace <azatibrakov@gmail.com>
2017-09-11 15:02:04 +0700

    Added 'guest session id' fixture

--- tests/fixtures/authentication.py
+++ tests/fixtures/authentication.py
@@ -4,7 +4,8 @@ from asyncio import (AbstractEventLoop,
 import pytest
 from aiohttp import ClientSession
 
-from asynctmdb.methods.authentication import create_request_token
+from asynctmdb.methods.authentication import (create_request_token,
+                                              create_guest_session)
 
 
 @pytest.fixture(scope='function')
@@ -17,6 +18,18 @@ def request_token(api_base_url: str,
                                                 session=session))
     response = event_loop.run_until_complete(future)
     return response['request_token']
+
+
+@pytest.fixture(scope='function')
+def guest_session_id(api_base_url: str,
+                     api_key: str,
+                     session: ClientSession,
+                     event_loop: AbstractEventLoop) -> str:
+    future = ensure_future(create_guest_session(api_base_url=api_base_url,
+                                                api_key=api_key,
+                                                session=session))
+    response = event_loop.run_until_complete(future)
+    return response['guest_session_id']
 
 
 @pytest.fixture(scope='session')
