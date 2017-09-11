7cf1f1f097d3a9eac631eb9ee769d131ae1c13bd
ace <azatibrakov@gmail.com>
2017-09-11 15:07:07 +0700

    Using 'StatusCode' enum for status codes assertions

--- tests/methods_tests/test_authentication.py
+++ tests/methods_tests/test_authentication.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pytest
 from aiohttp import ClientSession
 
+from asynctmdb.common import StatusCode
 from asynctmdb.methods.authentication import (create_request_token,
                                               create_session,
                                               create_guest_session)
@@ -13,20 +14,22 @@ async def test_create_request_token(api_base_url: str,
                                     api_key: str,
                                     invalid_api_key: str,
                                     session: ClientSession) -> None:
-    invalid_response = await create_request_token(api_base_url=api_base_url,
-                                                  api_key=invalid_api_key,
-                                                  session=session)
-    valid_response = await create_request_token(api_base_url=api_base_url,
-                                                api_key=api_key,
-                                                session=session)
+    invalid_api_key_response = await create_request_token(
+        api_base_url=api_base_url,
+        api_key=invalid_api_key,
+        session=session)
+    valid_response = await create_request_token(
+        api_base_url=api_base_url,
+        api_key=api_key,
+        session=session)
 
-    status_code = invalid_response['status_code']
+    invalid_api_key_status_code = invalid_api_key_response['status_code']
     succeed = valid_response['success']
     expiration_date_time = valid_response['expires_at']
     request_token = valid_response['request_token']
 
-    assert status_code == 7
-    assert succeed
+    assert invalid_api_key_status_code == StatusCode.INVALID_API_KEY
+    assert isinstance(succeed, bool) and succeed
     assert expiration_date_time >= datetime.utcnow()
     assert isinstance(request_token, str)
 
@@ -43,7 +46,7 @@ async def test_create_session(api_base_url: str,
 
     status_code = invalid_response['status_code']
 
-    assert status_code == 17
+    assert status_code == StatusCode.SESSION_DENIED
 
 
 @pytest.mark.asyncio
@@ -51,19 +54,21 @@ async def test_create_guest_session(api_base_url: str,
                                     api_key: str,
                                     invalid_api_key: str,
                                     session: ClientSession) -> None:
-    invalid_response = await create_guest_session(api_base_url=api_base_url,
-                                                  api_key=invalid_api_key,
-                                                  session=session)
-    valid_response = await create_guest_session(api_base_url=api_base_url,
-                                                api_key=api_key,
-                                                session=session)
+    invalid_api_key_response = await create_guest_session(
+        api_base_url=api_base_url,
+        api_key=invalid_api_key,
+        session=session)
+    valid_response = await create_guest_session(
+        api_base_url=api_base_url,
+        api_key=api_key,
+        session=session)
 
-    status_code = invalid_response['status_code']
+    invalid_api_key_status_code = invalid_api_key_response['status_code']
     succeed = valid_response['success']
     expiration_date_time = valid_response['expires_at']
     guest_session_id = valid_response['guest_session_id']
 
-    assert status_code == 7
+    assert invalid_api_key_status_code == StatusCode.INVALID_API_KEY
     assert succeed
     assert expiration_date_time >= datetime.utcnow()
     assert isinstance(guest_session_id, str)
