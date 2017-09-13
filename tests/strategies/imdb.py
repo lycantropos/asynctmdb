d5ce8b047eba950cc6f44f7e6cd037f23376c843
Azat Ibrakov <azatibrakov@gmail.com>
2017-09-13 12:12:57 +0700

    Restricting invalid IMDb ids to ASCII printable characters

--- tests/strategies/imdb.py
+++ tests/strategies/imdb.py
@@ -2,6 +2,6 @@ import string
 
 from hypothesis import strategies
 
-invalid_imdb_ids_characters = strategies.characters(
-        blacklist_characters='t' + string.digits)
+invalid_imdb_ids_characters = strategies.sampled_from(
+        sorted(set(string.printable) - {'t'}))
 invalid_imdb_ids = strategies.text(alphabet=invalid_imdb_ids_characters)
